6519ff0bb9e6b977a50a726ac3d0a82c07beefa2
Alain Chabrier <38245448+achabrier@users.noreply.github.com>
2020-09-16 12:31:40 +0200

    better diet model

--- diet/model.py
+++ diet/model.py
@@ -1,52 +1,53 @@
-#dd-cell
-food = inputs['diet_food']
-nutrients = inputs['diet_nutrients']
-food_nutrients = inputs['diet_food_nutrients']
-food_nutrients.set_index('Food', inplace=True)
-#dd-cell
+
+df_food = inputs['diet_food']
+foods = df_food['name'].values.tolist()
+df_food.set_index('name', inplace=True)
+
+df_nutrients = inputs['diet_nutrients']
+nutrients = df_nutrients['name'].values.tolist()
+df_nutrients.set_index('name', inplace=True)
+
+df_food_nutrients = inputs['diet_food_nutrients']
+df_food_nutrients.set_index('Food', inplace=True)
+
 from docplex.mp.model import Model
 
 # Model
 mdl = Model(name='diet')
 
-# Create decision variables, limited to be >= Food.qmin and <= Food.qmax
-qty = food[['name', 'qmin', 'qmax']].copy()
-qty['var'] = qty.apply(lambda x: mdl.continuous_var(lb=x['qmin'],
-                                                    ub=x['qmax'],
-                                                    name=x['name']),
-                       axis=1)
-# make the name the index
-qty.set_index('name', inplace=True)
+# Create variables
+qty = mdl.continuous_var_dict(foods, name='qty')
+
+# Limit range of foods
+for f in foods:
+    mdl.add_range(df_food.qmin[f], qty[f], df_food.qmax[f])
 
 # Limit range of nutrients, and mark them as KPIs
-for n in nutrients.itertuples():
-    amount = mdl.sum(qty.loc[f.name]['var'] * food_nutrients.loc[f.name][n.name]
-                     for f in food.itertuples())
-    mdl.add_range(n.qmin, amount, n.qmax)
-    mdl.add_kpi(amount, publish_name='Total %s' % n.name)
+for n in nutrients:
+    amount = mdl.sum(qty[f] * df_food_nutrients.loc[f][n] for f in foods)
+    mdl.add_range(df_nutrients.qmin[n], amount, df_nutrients.qmax[n])
+    mdl.add_kpi(amount, publish_name='Total %s' % n)
 
 # Minimize cost
-obj = mdl.sum(qty.loc[f.name]['var'] * f.unit_cost for f in food.itertuples())
+obj = mdl.sum(qty[f] * df_food.unit_cost[f] for f in foods)
 mdl.add_kpi(obj, publish_name="Minimal cost");
 mdl.minimize(obj)
 
 mdl.print_information()
-#dd-markdown <h1>Solve</h1>
-#dd-cell
+
+# solve
 ok = mdl.solve()
-#dd-cell
+
 mdl.print_solution()
-#dd-markdown Make dataframe from solution
-#dd-cell
+
 import pandas
 import numpy
 
-solution_df = pandas.DataFrame(columns=['name', 'value'])
+solution_df = pandas.DataFrame(columns=['Food', 'value'])
 
 for index, dvar in enumerate(mdl.solution.iter_variables()):
-    solution_df.loc[index,'name'] = dvar.to_string()
-    solution_df.loc[index,'value'] = dvar.solution_value
-#dd-cell
-solution_df
-#dd-cell
+    solution_df.loc[index, 'Food'] = dvar.to_string()
+    solution_df.loc[index, 'value'] = dvar.solution_value
+
+outputs = {}
 outputs['solution'] = solution_df
